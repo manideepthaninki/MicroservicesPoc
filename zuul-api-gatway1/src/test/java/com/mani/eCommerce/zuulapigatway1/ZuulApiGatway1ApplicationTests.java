515795a23935ddaf45e0891d5eda79a719bb5d48
manideepthaninki <78657731+manideepthaninki@users.noreply.github.com>
2021-05-31 13:34:55 +0530

    Added zuul gateway

---
+++ zuul-api-gatway1/src/test/java/com/mani/eCommerce/zuulapigatway1/ZuulApiGatway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mani.eCommerce.zuulapigatway1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
